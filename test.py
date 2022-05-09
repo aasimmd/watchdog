7721fbb9770faa43badf9aa3a3a989553bffcba5
aasimmd <aasim.md00@gmail.com>
2022-05-09 18:10:25 +0530

    fixed directory and file name issue, fixed created time

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 from time import sleep
 from datetime import datetime
-from venv import create
 import pandas as pd
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+
+def autofill():
+    try:
+        f = open('log.csv')
+        f.close()
+    except:
+        logdf = pd.DataFrame({'FileName':[],'CreatedTime':[],'ModifiedTime':[],'FileLocation':[],'Event':[]})
+        logdf.to_csv('log.csv')
 
 class Handler(FileSystemEventHandler):
     # def on_any_event(event):
@@ -15,51 +22,54 @@ class Handler(FileSystemEventHandler):
     
     @staticmethod
     def on_created(event):
-        global i
         print(f"{event.src_path} created")
         filename = event.src_path[index:]
         created_time = datetime.now()
         modified_time = datetime.now()
-        logdf.loc[i] = [filename, created_time, modified_time, path, "created"]
-        i+=1
+        logdf.loc[len(logdf.index)] = [filename, created_time, modified_time, path, "created"]
 
     @staticmethod
     def on_deleted(event):
-        global i
         print(f"deleted {event.src_path}!")
         filename = event.src_path[index:]
-        created_time = datetime.now()
+        try:
+            created_time = logdf.loc[logdf['FileName'] == filename]['CreatedTime'][0]
+        except:
+            created_time = datetime.now()
         modified_time = datetime.now()
-        logdf.loc[i] = [filename, created_time, modified_time, path, "deleted"]
-        i+=1
+        logdf.loc[len(logdf.index)] = [filename, created_time, modified_time, path, "deleted"]
 
     @staticmethod
     def on_modified(event):
-        global i
-        print(f"{event.src_path} has been modified")
-        filename = event.src_path[index:]
-        created_time = datetime.now()
-        modified_time = datetime.now()
-        logdf.loc[i] = [filename, created_time, modified_time, path, "modified"]
-        i+=1
+        if not event.is_directory:
+            print(f"{event.src_path} has been modified")
+            fpath = event.src_path
+            index = fpath.rfind("\\")
+            filename = fpath[index+1:]
+            path = fpath[:index]
+            try:
+                created_time = logdf.loc[logdf['FileName'] == filename]['CreatedTime'][0]
+            except:
+                created_time = datetime.now()
+            modified_time = datetime.now()
+            logdf.loc[len(logdf.index)] = [filename, created_time, modified_time, path, "modified"]
 
     @staticmethod
     def on_moved(event):
-        global i
         print(f"moved {event.src_path} to {event.dest_path}")
         filename = event.dest_path[index:]
-        created_time = datetime.now()
+        try:
+            created_time = logdf.loc[logdf['FileName'] == filename]['CreatedTime'][0]
+        except:
+            created_time = datetime.now()
         modified_time = datetime.now()
-        logdf.loc[i] = [filename, created_time, modified_time, path, f"moved from {event.src_path[index:]} to {event.dest_path[index:]}"]
-        i+=1
-
-# logdf = pd.DataFrame({'FileName':[],'CreatedTime':[],'ModifiedTime':[],'FileLocation':[],'Event':[]})
-# logdf.to_csv('log.csv', index=False)
-logdf = pd.read_csv('log.csv', index_col=[0])
+        logdf.loc[len(logdf.index)] = [filename, created_time, modified_time, path, f"moved from {event.src_path[index:]} to {event.dest_path[index:]}"]
+        
+autofill()
+logdf = pd.read_csv('log.csv', index_col=0)
 print(logdf)
 path="F:/temp/dir"
 index = len(path)+1
-i=len(logdf.index)
 event_handler = Handler()
 observer = Observer()
 observer.schedule(event_handler, path, recursive=True)
